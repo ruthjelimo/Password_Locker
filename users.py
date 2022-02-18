d496c5801e7e192c6b3b38e0f9a8d981360b940e
ruthjelimo <ruthjmimo@gmail.com>
2022-02-18 23:17:37 +0300

    added a function

--- users.py
+++ users.py
@@ -4,3 +4,7 @@ class User:
     def __init__(self,username,password):
         self.username=username
         self.password=password
+    
+    #create account
+    def save_account(self):
+        User.user_list.append(self)
