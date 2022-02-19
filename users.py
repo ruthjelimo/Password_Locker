383150e9e2c434bed0562fe63a77ffe45987f41a
ruthjelimo <ruthjmimo@gmail.com>
2022-02-19 13:44:46 +0300

    added docstring

--- users.py
+++ users.py
@@ -1,17 +1,26 @@
 class User:
+    '''
+    class that generates an instance of user
+    '''
     user_list=[]
    
     def __init__(self,username,password):
         self.username=username
         self.password=password
-    
+    """"
+    function that defines properties of a class
+    """
     
     def save_user(self):
         User.user_list.append(self)
-
+    """"
+    function to check if the user is saved
+    """
     def delete_user(self):
         User.user_list.remove(self)
-    
+    """"
+    function to check if the user is deleted
+    """
     @classmethod
     def find_user(cls, username):
         for user in cls.user_list:
--- users_test.py
+++ users_test.py
@@ -1,30 +1,47 @@
 #!usr/bin/env python3.8
 import unittest
+import pyperclip
 from users import User
 
 class TestUser(unittest.TestCase):
    def setUp(self):
-     self.new_user = User("jelimo","12345") 
-
+       """
+         Test class that defines test cases for the user class behaviours.
+        """
+   
+   
+       self.new_user = User("jelimo","12345") 
    
    def test_init(self):
-
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
         self.assertEqual(self.new_user.username,"jelimo")
         self.assertEqual(self.new_user.password,"12345")
-
+    
    def test_save_user(self):
-        
+        '''
+         test_save_user test case to test if the user object is saved into
+         the user list
+        '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
 
    def tearDown(self):
-       User.user_list =[]
+          '''
+            tearDown method that does clean up after each test case has run.
+            '''
+          User.user_list =[]
 
    def test_save_multiple_users(self):
-        self.new_user.save_user()
-        test_user = User("ruth","45678")
-        test_user.save_user()
-        self.assertEqual(len(User.user_list),2)
+         '''
+            test_save_multiple_users to check if we can save multiple users
+            objects to our user_list
+            '''
+         self.new_user.save_user()
+         test_user = User("ruth","45678")
+         test_user.save_user()
+         self.assertEqual(len(User.user_list),2)
    
    def  test_delete_user(self):
          self.new_user.save_user()
