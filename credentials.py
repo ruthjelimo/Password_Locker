ef606edd503c95ec93f550e6dbb7776465d1aa67
ruthjelimo <ruthjmimo@gmail.com>
2022-02-19 20:08:13 +0300

    added properties of a class

--- credentials.py
+++ credentials.py
@@ -3,4 +3,16 @@ class Credentials:
     class that generates an instance of credentials
     '''
      credentials_list=[]
-     
+
+def __init__(self,username,password):
+      """"
+    function that defines properties of a class
+    """
+      self.username = username
+      self.password = password
+
+def save_credentials(self):
+      """"
+    function to check if the user is saved
+    """
+      Credentials.credentials_list.append(self)
