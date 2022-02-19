cd97dd85afdb6dbdc3ed235781fdc6277dee3af6
ruthjelimo <ruthjmimo@gmail.com>
2022-02-19 20:22:47 +0300

    added test case for objects

--- credentials_test.py
+++ credentials_test.py
@@ -2,3 +2,19 @@
 import unittest
 import pyperclip
 from credentials import Credentials
+
+class TestUser(unittest.TestCase):
+   def setUp(self):
+       """
+         Test class that defines test cases for the credentials class behaviours.
+        """
+   
+   
+       self.new_credentials = Credentials("Talia","12345") 
+      
+   def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.username,"Talia")
+        self.assertEqual(self.new_user.password,"12345")
