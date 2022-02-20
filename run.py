85724f98b2bdd4bf0174cb6360522265622c5a2d
ruthjelimo <ruthjmimo@gmail.com>
2022-02-20 09:30:04 +0300

    added save function

--- run.py
+++ run.py
@@ -1,2 +1,16 @@
 #!/usr/bin/envi python3.8
 import pyperclip
+from users import User
+from credential import Credential
+def create_user(username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(username,password)
+    return new_user
+def save_user(self):
+      """"
+      function to check if the user is saved
+      """
+      User.user_list.append(self)
+    
